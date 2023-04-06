388619fac6e508ae7cb91b8c24b34b578efddd85
ghulammuhammad1 <ghulammuhammad.cs17@iba-suk.edu.pk>
2023-04-06 12:33:38 +0500

    modified app.js file

--- React/reactapp1/myapp/src/App.js
+++ React/reactapp1/myapp/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React here
+          Learn React here1
         </a>
       </div>
     </div>
